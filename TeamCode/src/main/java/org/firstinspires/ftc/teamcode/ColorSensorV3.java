62929694db5e6e8d396a5882aeeffbf9b7e82d4f
Claudiu Spiescu <claudiuspiescu@gmail.com>
2023-07-13 13:15:59 +0300

    Add RevColorSensorV3 function

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorV3.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+public class ColorSensorV3 extends Main {
+    String detectedColor;
+    int alphaValue;
+    public String getColor(ColorSensor colorSensor) {
+        if (colorSensor.red() > colorSensor.green()) {
+            if (colorSensor.red() > colorSensor.blue()) {
+                detectedColor = "Red";
+            } else if (colorSensor.blue() > colorSensor.green()) {
+                detectedColor = "Blue";
+            }
+        } else if (colorSensor.green() > colorSensor.blue()) {
+            detectedColor = "Green";
+        } else {
+            detectedColor = "Blue";
+        }
+
+        return detectedColor;
+    }
+/*
+    public int getAlpha() {
+        colorSensor = hardwareMap.get(RevColorSensorV3.class, "colorSensor");
+        alphaValue = colorSensor.alpha();
+        return alphaValue;
+    }*/
+}
