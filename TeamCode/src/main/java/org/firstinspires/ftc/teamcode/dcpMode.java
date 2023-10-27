c125350252ad08d209d5c236b32f407a5e865aa3
PatrikDav <davidpatrik253@gmail.com>
2023-10-27 15:25:26 +0300

    Create Driver Controlled Period file + function movementWheels+function robotArmController

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/dcpMode.java
@@ -11,52 +11,46 @@ public class dcpMode extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         waitForStart();
-
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
           movementWheels();
+          robotArmController();
         }
     }
 
 
     public void movementWheels()
     {
-        //NU VA BAGATI DEGETELE AICI !!!
-        // Declare our motors
-        // Make sure your ID's match your configuration
+        // Motor declaration
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
 
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead.
-        // See the note about this earlier on this page.
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-        double rx = gamepad1.right_stick_x;
+        // Read values from gamepad 1
+        double gp1Y = -gamepad1.left_stick_y;
+        double gp1X = -gamepad1.left_stick_x * 1.1;
+        double gp1RotationX = gamepad1.right_stick_x;
 
-        // Denominator is the largest motor power (absolute value) or 1
-        // This ensures all the powers maintain the same ratio,
-        // but only if at least one is out of the range [-1, 1]
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double frontLeftPower = (y + x + rx) / denominator;
-        double backLeftPower = (y - x + rx) / denominator;
-        double frontRightPower = (y - x - rx) / denominator;
-        double backRightPower = (y + x - rx) / denominator;
+        // Calculate the engines power
+        double denominator = Math.max(Math.abs(gp1Y) + Math.abs(gp1X) + Math.abs(gp1RotationX), 1);
+        double frontLeftPower = (gp1Y + gp1X - gp1RotationX) / denominator;
+        double backLeftPower = (gp1Y - gp1X - gp1RotationX) / denominator;
+        double frontRightPower = (gp1Y - gp1X + gp1RotationX) / denominator;
+        double backRightPower = (gp1Y + gp1X + gp1RotationX) / denominator;
 
+        // Assign values to the engines
         frontLeftMotor.setPower(frontLeftPower);
         backLeftMotor.setPower(backLeftPower);
         frontRightMotor.setPower(frontRightPower);
         backRightMotor.setPower(backRightPower);
     }
 
-    public void commandsRobotController()
+    public void robotArmController()
     {
         double extendVipers=gamepad1.right_trigger;
         double retractVipers=-gamepad1.left_trigger;
