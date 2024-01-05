6bc49b7f7919d8c476e9e841b6cb91e3d8a4b3a1
PatrikDav <davidpatrik253@gmail.com>
2024-01-05 12:42:45 +0200

    small updates in ArmController class, involving the speed of the viper's extension

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
@@ -6,59 +6,16 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.subsystems.ChassyController;
 //endregion
 
 @Autonomous(name = "TestAutonom1")
 public class AutonomousMode extends LinearOpMode {
-    HardwareInit rd1 = new HardwareInit();
-
-    // Drive function with 3 parameters
-    private void drive(double power, int frontRightTicks, int frontLeftTicks, int rearRightTicks, int rearLeftTicks) {
-        int frontRightTarget;
-        int frontLeftTarget;
-        int rearLeftTarget;
-        int rearRightTarget;
-
-        if (opModeIsActive()) {
-            // Create target positions
-            frontRightTarget = rd1.frontRightMotor.getCurrentPosition() + frontRightTicks;
-            frontLeftTarget = rd1.frontLeftMotor.getCurrentPosition() + frontLeftTicks;
-            rearRightTarget = rd1.rearRightMotor.getCurrentPosition() + rearRightTicks;
-            rearLeftTarget = rd1.rearLeftMotor.getCurrentPosition() + rearLeftTicks;
-
-            // set target position
-            rd1.frontRightMotor.setTargetPosition(frontLeftTarget);
-            rd1.frontLeftMotor.setTargetPosition(frontRightTarget);
-            rd1.rearRightMotor.setTargetPosition(rearLeftTarget);
-            rd1.rearLeftMotor.setTargetPosition(rearRightTarget);
-
-            //switch to run to position mode
-            rd1.frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            rd1.frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            rd1.rearRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            rd1.rearLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            //run to position at the designated power
-            rd1.frontRightMotor.setPower(power);
-            rd1.frontLeftMotor.setPower(power);
-            rd1.rearRightMotor.setPower(power);
-            rd1.rearLeftMotor.setPower(power);
-
-            // wait until all motors are no longer busy running to position
-            while (opModeIsActive() && (rd1.frontRightMotor.isBusy() || rd1.frontLeftMotor.isBusy() || rd1.rearLeftMotor.isBusy() || rd1.rearRightMotor.isBusy())) {
-                idle();
-            }
-
-            // set motor power back to 0
-            rd1.frontRightMotor.setPower(0);
-            rd1.frontLeftMotor.setPower(0);
-            rd1.rearRightMotor.setPower(0);
-            rd1.rearLeftMotor.setPower(0);
-        }
-    }
 
     @Override
     public void runOpMode() {
+        HardwareInit rd1 = new HardwareInit();
         rd1.init(hardwareMap);
 
         waitForStart();
@@ -67,8 +24,28 @@ public class AutonomousMode extends LinearOpMode {
         if (opModeIsActive()) {
             // wait for team prop detection
 
-            //segment 1
-            drive(0.7, 30, 15, 30, 15);
+            //segment 1 (first pixel placement)
+            /*
+            if(){
+                //PROP is in the 1st segment of the image
+                ChassyController.drive(0.7, 30, 15, 30, 15);
+            }else if(){
+                //PROP is in the 2nd segment of the image
+                ChassyController.drive(0.7, 30, 15, 30, 15);
+            }else if(){
+                //PROP is in the 3rd segment of the image
+                ChassyController.drive(0.7, 30, 15, 30, 15);
+            }
+            */
+
+            //TO GO LEFT YOU HAVE TO GIVE 1/2 of TICKS TO THE LEFT WHEELS
+            //EX: ChassyController.drive(0.7, 500, 250, 500, 250);
+
+            //TO GO RIGHT YOU HAVE TO GIVE 1/2 of TICKS TO THE RIGHT WHEELS
+            //EX: ChassyController.drive(0.7, 250, 500, 250, 500);
+
+            //segment 2
+            ChassyController.drive(0.7, 30, 15, 30, 15);
 
 
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ChassyController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ChassyController.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+
+import org.firstinspires.ftc.teamcode.HardwareInit;
 // endregion
 
 public class ChassyController {
@@ -28,4 +30,48 @@ public class ChassyController {
         frontRightMotor.setPower(frontRightPower);
         rearRightMotor.setPower(rearRightPower);
     }
+
+    public static void drive(double power, int frontRightTicks, int frontLeftTicks, int rearRightTicks, int rearLeftTicks) {
+        HardwareInit rd1 = new HardwareInit();
+
+        int frontRightTarget;
+        int frontLeftTarget;
+        int rearLeftTarget;
+        int rearRightTarget;
+
+        // Create target positions
+        frontRightTarget = rd1.frontRightMotor.getCurrentPosition() + frontRightTicks;
+        frontLeftTarget = rd1.frontLeftMotor.getCurrentPosition() + frontLeftTicks;
+        rearRightTarget = rd1.rearRightMotor.getCurrentPosition() + rearRightTicks;
+        rearLeftTarget = rd1.rearLeftMotor.getCurrentPosition() + rearLeftTicks;
+
+        // set target position
+        rd1.frontRightMotor.setTargetPosition(frontLeftTarget);
+        rd1.frontLeftMotor.setTargetPosition(frontRightTarget);
+        rd1.rearRightMotor.setTargetPosition(rearLeftTarget);
+        rd1.rearLeftMotor.setTargetPosition(rearRightTarget);
+
+        //switch to run to position mode
+        rd1.frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rd1.frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rd1.rearRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rd1.rearLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //run to position at the designated power
+        rd1.frontRightMotor.setPower(power);
+        rd1.frontLeftMotor.setPower(power);
+        rd1.rearRightMotor.setPower(power);
+        rd1.rearLeftMotor.setPower(power);
+
+        // wait until all motors are no longer busy running to position
+        while (rd1.frontRightMotor.isBusy() || rd1.frontLeftMotor.isBusy() || rd1.rearLeftMotor.isBusy() || rd1.rearRightMotor.isBusy()) {
+            // do nothing
+        }
+
+        // set motor power back to 0
+        rd1.frontRightMotor.setPower(0);
+        rd1.frontLeftMotor.setPower(0);
+        rd1.rearRightMotor.setPower(0);
+        rd1.rearLeftMotor.setPower(0);
+        }
 }
